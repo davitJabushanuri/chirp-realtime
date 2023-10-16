6e318c541a197827ecd81fed477da53d3f8f28f1
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-10-16 20:47:50 +0400

    chore: upgrade packages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
 import express from "express";
+import fs from "fs";
 const app = express();
 
+fs.readFile("./src/lorem.txt", "utf8", (err, data) => {
+	if (err) throw err;
+	console.log(data);
+});
+
+fs.appendFile("./src/lorem.txt", "appended", (err) => {
+	if (err) throw err;
+	console.log("File appended");
+});
+
+fs.writeFile("./src/lorem.txt", "Hello World!", (err) => {
+	if (err) throw err;
+	console.log("File written");
+});
+
 app.get("/", (req, res) => {
-  res.send("hello");
+	res.send("hello");
 });
 
 app.listen(8080, () => {
-  console.log("Server started on port 3000");
+	console.log("Server started on port 8080");
 });
