e4fe4444fe4c87616c5e6da87db4062c35f75cd1
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-01-07 00:27:28 +0400

    test staging server

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-	res.send("Hello from staging!");
+	res.send("This is a staging server");
 });
 
 app.get("/api", (req, res) => {
