87b1b2cc639d816ee98682991c947e6ff8550efa
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-02-06 00:28:34 +0400

    create upload image util function

---
+++ src/utils/upload-image.ts
@@ -0,0 +1,26 @@
+import { supabase } from "@/utils/supabase";
+import { createId } from "@paralleldrive/cuid2";
+
+export const postImage = async (file: File, bucket: string) => {
+	try {
+		const imagePath = createId();
+
+		const { error } = await supabase.storage
+			.from(bucket)
+			.upload(`${bucket}-${imagePath}`, file, {
+				cacheControl: "3600",
+				upsert: false,
+			});
+		if (error) {
+			throw new Error(error.message);
+		} else {
+			const { data: mediaUrl } = supabase.storage
+				.from(bucket)
+				.getPublicUrl(`${bucket}-${imagePath}`);
+
+			return { url: mediaUrl?.publicUrl, path: imagePath };
+		}
+	} catch (error: unknown) {
+		throw new Error("Error uploading image");
+	}
+};
