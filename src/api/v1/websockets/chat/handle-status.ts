d5dc0acf92b6735bb686bf625a76ad4c3bc81f11
davitJabushanuri <davitjabushanuri95@gmail.com>
2024-05-16 23:19:55 +0400

    refactor handle-status functionality

--- src/api/v1/websockets/chat/handle-status.ts
+++ src/api/v1/websockets/chat/handle-status.ts
@@ -1,28 +1,29 @@
-import type { SocketEmitStatusPayload } from "@/types";
-import type { Socket } from "socket.io";
-import { prisma } from "@/lib/prisma";
 import { logger } from "@/utils/logger";
+import { prisma } from "@/lib/prisma";
+import type { Socket } from "socket.io";
+import type { IStatus } from "./types";
 
-export const handleStatus = async (
-	socket: Socket,
-	data: SocketEmitStatusPayload,
-) => {
-	if (data.status === "sent" || data.status === "failed") {
-		socket.emit("status", data);
-	} else {
-		socket.broadcast.to(socket.conversation_id).emit("status", data);
+export const handleStatus = async (socket: Socket, data: IStatus) => {
+	try {
+		if (data.status === "sent" || data.status === "failed") {
+			socket.emit("status", data);
+		} else {
+			socket.broadcast.to(socket.conversation_id).emit("status", data);
 
-		try {
 			await prisma.message.update({
 				where: { id: data.message_id },
 				data: { status: "seen" },
 			});
-		} catch (error) {
-			if (error instanceof Error) {
-				logger.error(error.message);
-			} else {
-				throw error;
-			}
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			logger.error(error.message);
+			socket.emit("error", {
+				message: "Failed to update message status",
+				error: error.message,
+			});
+		} else {
+			throw error;
 		}
 	}
 };
